d4d2a01a83698c875de2c294199b1661a831769f
Alexis Lapasset <alexis.lapasset@gmail.com>
2020-05-20 15:09:05 +0200

    temp

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div v-for="user of users" :key="user.id">
+      <p>{{ user.name }}</p>
+      <p>{{ user.email }}</p>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      users: []
+    }
+  },
+  methods: {
+    fetchUsers: function () {
+      const baseURI = 'https://jsonplaceholder.typicode.com/users'
+      this.$http.get(baseURI)
+        .then((result) => {
+          this.users = result.data;
+        })
+    }
   }
 }
 </script>
